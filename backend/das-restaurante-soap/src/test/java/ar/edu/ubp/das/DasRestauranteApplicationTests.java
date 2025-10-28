656eed463bff2eca5c5bcecd3d1f05cc8dc1fa9b
Maximo Perez <maximo@wyeworks.com>
2025-10-27 21:57:53 -0300

    Init das-restaurante soap

---
+++ backend/das-restaurante-soap/src/test/java/ar/edu/ubp/das/DasRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.ubp.das;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DasRestauranteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
